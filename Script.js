f636aa38741bfc62dcd1587e07149b34171dd374
Chethan G P <chethu8050612162@gmail.com>
2026-09-15 16:47:38 +0530

    Logical Thinking Questions

--- Script.js
+++ Script.js
@@ -487,3 +487,29 @@
 
 // ---------------------------------------------------------------------
 
+// ## Logical Thinking Questions
+
+// // 1. Take two numbers and print which one is greater.
+// // 2. Check whether a number lies between 10 and 50.
+// // 3. Check whether a password length is greater than 8.
+//     var num = 12345678;
+//     if (num/10000000 >=1 )
+//         console.log("geater")
+//     else
+//         console.log("lesser")
+
+// // 4. Check if a person can drive:
+// // - age > 18
+// // - has license = true
+// // 1. Check whether a number is divisible by 2, 3, or both.
+// // 2. Print `"Good Morning"`, `"Good Afternoon"`, or `"Good Evening"` based on time.
+// // 3. Find whether a number is a multiple of 10.
+// // 4. Create a simple discount calculator.
+//     function calc(price,dis){
+//         return ((price* (100-dis))/100);
+//     }
+
+//     console.log("$ "+ calc(100,10)+ " Only")
+
+// 5. Check whether a product is in stock.
+// 6. Calculate final bill after GST.
